1d4e0a5dd5c2dc40b1b230ea448edff3be9b3b24
Ahmed Masud <ahmed.masud@trustifier.com>
2017-01-20 20:46:34 -0500

    fixed version number to reflect tag

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Very basic install over net script
 
-VERSION=v1.0.0
+VERSION=v1.0.1
 FILELIST="Makefile scripts/help.pl scripts/spin-tee.sh scripts/spinner.sh"
 mkdir -p scripts
 for file in ${FILELIST} ; do
